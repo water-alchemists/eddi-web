3badf4cf4d4bf5e0cd7c17c974630d6eac4d7465
Tony <terimyaki@users.noreply.github.com>
2015-12-19 15:29:26 -0500

    removed unnecessary gulp and added sqlite3, sequelize

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var gulp = require('gulp'),
-	browserify = require('browserify'),
-	source = require('vinyl-source-stream'),
-	rename = require('gulp-rename');
-
-gulp.task('buildArtik', function(){
-	
-})
