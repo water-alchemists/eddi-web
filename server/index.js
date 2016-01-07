047ca54fb3941b3b095397546bbb72d3f972617c
Jesse <mienaikoe@gmail.com>
2016-01-07 00:23:45 -0500

    adding views

--- server/index.js
+++ server/index.js
@@ -4,14 +4,32 @@ var routes = require('./routes'),
 	middlewares = require('./middlewares'),
 	models = require('./models');
 
-var PORT = 3991,
-	app = express();
+var PORT = 3991;
+var app = express();
+app.set('view engine', 'jade');
 
 routes(app);
 middlewares(app);
 
 app.get('/', function(req, res){
-	res.send('hello world');
+	res.render('eddi-main', {
+		cards: [
+			{
+				title: "Flow Rate",
+				value: "3.2",
+				unit: "liters per minute"
+			},{
+				title: "Salinity In",
+				value: "1024",
+				unit: "parts per million"
+			},{
+				title: "Salinity Out",
+				value: "387",
+				unit: "parts per million"
+			}
+		],
+		title: "Greenhouse 1",
+	});
 });
 
 models.sequelize.sync().then(() => {
